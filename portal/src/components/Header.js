4441f0365f71d4e7d34f6f8c36fd07b73f07677e
Brinicle177 <kimkr0421@naver.com>
2023-01-11 17:38:23 +0900

    헤더 완성 /설아

---
+++ portal/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import '../css/header.css'
+import logo from '../asset/img/logo_sbl.png'
+import profileImg from '../asset/img/img_user.PNG'
+
+const Header = () => {
+
+
+    let name = '체크'
+
+    return (
+        <div className='headerTopDiv'>
+            <div className='headerInnerDiv'>
+                <img className='headerLogo' src={logo} />
+
+                <div className='headerRight'>
+                    <p>강예진 연구원</p>
+                    <img className='headerImgProfile' src={profileImg} />
+                    <button className='headerBtn'>로그아웃</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
