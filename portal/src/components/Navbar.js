b18b2c160c419c99c133f3799ff51fc699d88c80
Brinicle177 <kimkr0421@naver.com>
2023-01-12 15:22:50 +0900

    navbar props 방식으로 변경 /설아

--- portal/src/components/Navbar.js
+++ portal/src/components/Navbar.js
@@ -3,14 +3,12 @@ import { useNavigate } from 'react-router-dom'
 
 import '../css/MainComponent.css'
 
-const Navbar = () => {
+const Navbar = ({menuList}) => {
 
   const navigate = useNavigate()
   const onClickNav = (e) => {
     navigate(e.currentTarget.getAttribute('url'))
   }
-
-  const menuList = [{title: "수업관리", subtitle: [{title: "공지사항", url: "/C_announcement"}, {title: "강의 일정", url: "/"}, {title: "공유 자료실", url: "/"}, {title: "연장사용 신청서", url: "/"}]}, {title: "프로젝트", subtitle: [{title: "프로젝트 관리", url: "/"}]}, {title: "학생관리", subtitle: [{title: "출결 관리", url: "/"}, {title: "학생 정보", url: "/"}]}]
 
   let navContent = menuList.map((item)=>(
   <div className='navContentBox' key={item.title}>
--- portal/src/pages/Course/Teacher/T_main.js
+++ portal/src/pages/Course/Teacher/T_main.js
@@ -12,12 +12,15 @@ import Schedule from '../../../components/main/Schedule';
 import Navbar from '../../../components/Navbar';
 
 const T_main = () => {
+
+  const menuList = [{title: "수업관리", subtitle: [{title: "공지사항", url: "/C_announcement"}, {title: "강의 일정", url: "/"}, {title: "공유 자료실", url: "/"}, {title: "연장사용 신청서", url: "/"}]}, {title: "프로젝트", subtitle: [{title: "프로젝트 관리", url: "/"}]}, {title: "학생관리", subtitle: [{title: "출결 관리", url: "/"}, {title: "학생 정보", url: "/"}]}]
+
   return (
     <div className='T_mainTopDiv'>
       <Header />
       <div className='T_mainInner'>
         <div className='T_mainInnerL'>
-          <Navbar/>
+          <Navbar menuList={menuList}/>
         </div>
         <div className='T_mainInnerR'>
           <Container>
