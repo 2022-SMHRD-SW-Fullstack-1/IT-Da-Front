fbb97b6fd240ed36bbc83e4e546cccf2b51d3975
Min-SungJo <123or@naver.com>
2023-01-16 17:37:38 +0900

    학생 라우터 경로 추가

--- portal/src/routes/S_Routes.js
+++ portal/src/routes/S_Routes.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import S_main from '../pages/Course/Student/S_main.js'
+import S_R_main from '../pages/Course/Student/S_R_main'
 
 const S_Routes = () => {
    return (
       <Routes>
          <Route path='/' element={<S_main />}></Route>
+         <Route path='/resume' element={<S_R_main />}></Route>
       </Routes>
    )
 }
