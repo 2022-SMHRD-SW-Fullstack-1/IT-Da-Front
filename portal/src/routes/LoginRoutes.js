75c0e79451cda59bc12fbcfb9f98c3cb5741517b
유성용 <suseong77@naver.com>
2023-01-19 17:44:21 +0900

    기업페이지 70%완성

--- portal/src/routes/LoginRoutes.js
+++ portal/src/routes/LoginRoutes.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Login from '../pages/Account/Login'
-import Register from '../pages/Account/Register'
-import E_Main from '../pages/Enterprise/E_main'
+import Login from "../pages/Account/Login";
+import Register from "../pages/Account/Register";
+import E_Main from "../pages/Enterprise/E_main";
+import E_main_detail from "../pages/Enterprise/E_main_detail";
 
 const LoginRoutes = () => {
-   return (
-      <Routes>
-         <Route path='/' element={<Login/>}/>
-         <Route path='/register' element={<Register/>}/>
-         <Route path='/e_main' element={<E_Main/>}/>
-      </Routes>
-   )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/e_main" element={<E_Main />} />
+      <Route path="/detail_user" element={<E_main_detail />} />
+    </Routes>
+  );
+};
 
-export default LoginRoutes
+export default LoginRoutes;
