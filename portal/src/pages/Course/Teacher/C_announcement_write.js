abd18939099ed881cbabc6da77d729baecb8874d
Brinicle177 <kimkr0421@naver.com>
2023-01-19 17:40:23 +0900

    공지사항 모든 기능 완성 및 글꼴 설정

--- portal/src/pages/Course/Teacher/C_announcement_write.js
+++ portal/src/pages/Course/Teacher/C_announcement_write.js
@@ -1,11 +1,18 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import axios from 'axios';
 import ReactQuill from 'react-quill';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import '../../../css/Course.css'
 import 'react-quill/dist/quill.snow.css';
 import writeBtn from '../../../asset/img/logo_sbl.png'
 
-const C_announcement_write = () => {
+const C_announcement_write = (props) => {
+
+   const navigate = useNavigate();
+
+   // navigate를 이용해 전달한 값 가져오기
+   const { state } = useLocation();
 
    // input value 가져오기
    const [title, setTitle] = useState('');
@@ -13,19 +20,51 @@ const C_announcement_write = () => {
       setTitle(e.target.value)
    };
    const [content, setContet] = useState('');
-   const onContentChange = e => {
-      setContet(e.target.value)
-   };
+   
+   useEffect(()=>{
+      if (state.title == '글 수정') {
+         setTitle(state.b_title)
+         setContet(state.b_content)
+      }
+   },[])
+
 
    // 글 작성 버튼 누르면 작성된 내용 가져오기
    const clickWirteBtn = () => {
       console.log(title)
       console.log(content)
+      if (state.title == '글 수정') {
+         axios
+         .post('/announcement/editPost', {
+            title: title,
+            content: content,
+            b_num: state.b_num,
+         })
+         .then((res) => {
+            console.log(res)
+            navigate('/announcement', { state: { state: title } })
+         })
+         .catch((e) => console.log(e));
+      } else {
+         axios
+         .post('/announcement/addPost', {
+            title: title,
+            content: content,
+            id: window.sessionStorage.getItem("loginId"),
+            key: window.sessionStorage.getItem("course_key"),
+         })
+         .then((res) => {
+            console.log(res)
+            navigate('/announcement', { state: { state: title } })
+         })
+         .catch((e) => console.log(e));
+      }
+      
    }
 
    return (
       <div className='container annContainer'>
-         <p>공지사항 글 수정</p>
+         <p>공지사항 {state.title}</p>
          <div className='content annContent'>
             <p>제목</p>
             <input onChange={onTitleChange} value={title} type='text'></input>
