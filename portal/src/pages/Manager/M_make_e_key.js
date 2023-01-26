0cd7e83810e0a2b9369fa4731e3825cc996d5a2a
zindol2 <yangjunho444@gmail.com>
2023-01-26 10:30:10 +0900

    관리자 기업 공고 생성, 기업 키생성 디자인

--- portal/src/pages/Manager/M_make_e_key.js
+++ portal/src/pages/Manager/M_make_e_key.js
@@ -1,49 +1,51 @@
-import axios from 'axios'
-import React, { useState, useRef } from 'react'
-import "../../css/M_main.css"
+import axios from 'axios';
+import React, { useState, useRef } from 'react';
+import '../../css/M_main.css';
 
 const M_make_e_key = () => {
+  const enter_nameRef = useRef('');
 
-    const enter_nameRef = useRef("");
+  const [enter_name, setEnter_name] = useState('');
 
-    const [enter_name, setEnter_name] = useState("")
+  const onEnter_name = (e) => {
+    setEnter_name(e.target.value);
+  };
 
-    const onEnter_name = e => {
-        setEnter_name(e.target.value)
-    }
+  const makeEnterprise = (e) => {
+    e.preventDefault();
+    axios
+      .post('/enterprise/make_enterprise', {
+        enter_name: enter_name,
+      })
+      .then(function (res) {
+        alert('기업생성 완료');
+        window.location.reload();
+      })
+      .catch(function (err) {
+        console.log('error');
+      });
+  };
 
-    const makeEnterprise = (e) => {
-        e.preventDefault();
-        axios
-            .post("/enterprise/make_enterprise", {
-                enter_name: enter_name,
-            })
-            .then(function (res) {
-                alert("기업생성 완료")
-                window.location.reload();
-            })
-            .catch(function (err) {
-                console.log("error")
-            })
-    }
-
-
-    return (
-        <div className='basic_container container'>
-            <p>과정생성</p>
-            <div className='course_make_container_inner content'>
-                <div className='container_inner_left'>
-                    <p>기업명</p>
-                </div>
-                <div className='container_inner_right'>
-                    <input type="text" value={enter_name} onChange={onEnter_name} ref={enter_nameRef} />
-                </div>
-            </div>
-            <div className='manager_button'>
-                <button onClick={makeEnterprise}>기업key 생성하기</button>
-            </div>
+  return (
+    <div className=" registerCntainer basic_container_container">
+      <div className="">
+        <p>기업 키 생성</p>
+        <div className="">
+          <span>기업 명</span>
+          <input
+            placeholder="입력해주세요."
+            type="text"
+            value={enter_name}
+            onChange={onEnter_name}
+            ref={enter_nameRef}
+          />
         </div>
-    )
-}
+        <button className="manager_button" onClick={makeEnterprise}>
+          기업 키 생성하기
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default M_make_e_key
+export default M_make_e_key;
