adf78d34f3bfa7b449b0a566e339a2d0d8e1fac4
Brinicle177 <kimkr0421@naver.com>
2023-01-27 09:53:30 +0900

    기업 회원가입 추가 및 관리자 로그인 코스키 추가

--- portal/src/pages/Account/Login.js
+++ portal/src/pages/Account/Login.js
@@ -4,10 +4,14 @@ import axios from 'axios';
 import '../../css/Login.css';
 
 const Login = () => {
+
   const navigate = useNavigate();
   const onClickRegister = () => {
     navigate('/register');
   };
+  const onClickE_Register = () => {
+    navigate('/e_register');
+  }
 
   const [tab, setTab] = useState(true);
 
@@ -21,12 +25,6 @@ const Login = () => {
   const [pw, setPw] = useState('');
   const onPwChange = (e) => {
     setPw(e.target.value);
-  };
-
-  const inputKey = useRef('');
-  const [key, setKey] = useState('');
-  const onKeyChange = (e) => {
-    setKey(e.target.value);
   };
 
   const onMLoginClick = () => {
@@ -54,7 +52,7 @@ const Login = () => {
           window.sessionStorage.setItem('loginId', res.data.mb_id);
           window.sessionStorage.setItem('role', 'a');
           window.sessionStorage.setItem('userName', res.data.mb_name);
-          window.sessionStorage.setItem('course_key', res.data.course_key);
+          window.sessionStorage.setItem('course_key', "52D8EECC");
           window.location.replace('/');
         } else alert('일치하는 회원정보가 없습니다');
       })
@@ -62,7 +60,6 @@ const Login = () => {
   };
 
   const onELoginClick = () => {
-    console.log(key);
     window.sessionStorage.setItem('role', 'e');
     window.sessionStorage.setItem('userName', '유티소프트');
     window.location.replace('/e_main');
@@ -73,27 +70,33 @@ const Login = () => {
       onMLoginClick(); // Enter 입력이 되면 클릭 이벤트 실행
     }
   };
+  const handleOnKeyPress2 = (e) => {
+    if (e.key === 'Enter') {
+      onELoginClick(); // Enter 입력이 되면 클릭 이벤트 실행
+    }
+  };
 
   return (
     <div className="loginContainer">
       <div className="topDiv">
         <div className="loginTab">
-          <button
+          <button className={tab?'loginBtnStyle':''}
             onClick={() => {
               setTab(true);
               setId('');
               setPw('');
             }}
           >
-            회원로그인
+            회원 로그인
           </button>
-          <button
+          <button  className={tab?'':'loginBtnStyle'}
             onClick={() => {
               setTab(false);
-              setKey('');
+              setId('');
+              setPw('');
             }}
           >
-            기업로그인
+            기업 로그인
           </button>
         </div>
         {tab ? (
@@ -130,20 +133,31 @@ const Login = () => {
         ) : (
           <div className="enterpriseLogin">
             <input
-              ref={inputKey}
-              onChange={onKeyChange}
-              value={key}
+              ref={inputId}
+              onChange={onIdChange}
+              value={id}
               type="text"
-              placeholder="키를 입력해주세요"
+              placeholder="아이디를 입력해주세요"
+            ></input>
+            <input
+              ref={inputPw}
+              onChange={onPwChange}
+              value={pw}
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              onKeyPress={handleOnKeyPress2}
             ></input>
             <div>
               <input type="checkbox"></input>
               <span>로그인 유지</span>
               <input type="checkbox"></input>
-              <span>키 저장</span>
+              <span>아이디 저장</span>
             </div>
             <button onClick={onELoginClick}>로그인</button>
-            <span>키 발급 문의</span>
+            <div>
+              <span>아이디 찾기</span>|<span>비밀번호 찾기</span>|
+              <span className='hoverHand' onClick={onClickE_Register}>회원가입</span>
+            </div>
           </div>
         )}
       </div>
