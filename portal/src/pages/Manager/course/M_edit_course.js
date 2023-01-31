b1c208b8e5b4cdc30e7a5e899ed353e455982fb1
Min-SungJo <123or@naver.com>
2023-01-31 16:33:56 +0900

    전체과정 필터링

--- portal/src/pages/Manager/course/M_edit_course.js
+++ portal/src/pages/Manager/course/M_edit_course.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import "../../../css/M_main.css"
+import "../../../css/filter.css"
 import axios from "axios"
 import M_course_list from "./M_course_list"
+import uuid from "react-uuid"
 
 const M_edit_course = (props) => {
 
@@ -12,19 +15,65 @@ const M_edit_course = (props) => {
             .then(function (res) {
                 setCourse(res.data)
                 console.log(res.data)
+                setNewCourse(res.data)
             })
             .catch(function (error) {
                 console.log("error")
             })
     }, [])
+    //필터기능
+    const onFilter = (f_course_campus,f_course_teacher,f_course_name,f_course_subject) => {
+        console.log("필터내용",filter)
+        //newCourse리스트를 course에서 필터링 내용이 포함된 리스트로 구성
+        setNewCourse(course.filter(course =>
+            course.course_campus.includes(f_course_campus)&&
+            course.course_teacher.includes(f_course_teacher)&&
+            course.course_name.includes(f_course_name)&&
+            course.course_subject.includes(f_course_subject)))
+      }
+    //필터input 태그 함수
+    const onChange = e => {
+        const { name, value } = e.target;
+        setFilter({
+            ...filter,
+            [name]: value
+        })
+    }
+
+    //필터관련 변수
+    const [filter, setFilter] = useState({
+        f_course_campus:'',
+        f_course_teacher:'',
+        f_course_name:'',
+        f_course_subject:'',
+    })
+    const {f_course_campus, f_course_teacher,f_course_name,f_course_subject} = filter
+    
+    const [newCourse, setNewCourse]=useState([{
+        course_subject:'',
+        course_name:'',
+        course_campus:'',
+        course_s_dt:'',
+        course_e_dt:'',
+        course_teacher:'',
+        course_limit:'',
+    }])
 
     const courseList =
-        course.map((item) => <M_course_list item={item} key={item.course_teacher} />)
+        newCourse.map((item) => <M_course_list item={item} key={uuid()} />)
 
     return (
         <div className='container'>
         <p>전체 과정</p>
+        <div className="filter_input_div">
+                <div>캠퍼스<input type='text' className="filter_input" name='f_course_campus' onChange={onChange}/></div>
+                <div>담임명<input type='text' className="filter_input" name='f_course_teacher' onChange={onChange}/></div>
+                <div>과정명<input type='text' className="filter_input" name='f_course_name' onChange={onChange}/></div>
+                <div>과정주제<input type='text' className="filter_input" name='f_course_subject' onChange={onChange}/></div>
+                <div><button onClick={()=>onFilter(f_course_campus, f_course_teacher, f_course_name,f_course_subject)}>검색</button></div>
+            </div>
              <div className='content'>
+                
                     <table>
                         <thead>
                             <tr>
