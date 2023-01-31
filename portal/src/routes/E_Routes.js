f4190874c71928c1f819b7e09da9dfd9f764dc29
유성용 <suseong77@naver.com>
2023-01-31 09:24:48 +0900

    라우터 수정

--- portal/src/routes/E_Routes.js
+++ portal/src/routes/E_Routes.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import E_main from "../pages/Enterprise/E_main";
 import E_main_detail from "../pages/Enterprise/E_main_detail";
-
+import E_t from "../pages/Enterprise/E_test";
+import E_main_bookmark from "../pages/Enterprise/E_main_bookmark";
 const E_Routes = () => {
-   return (
-      <Routes>
-         <Route path='/' element={<E_main />}></Route>
-         <Route path="/detail_user" element={<E_main_detail />} />
-      </Routes>
-   )
-}
-
-export default E_Routes
+  return (
+    <Routes>
+      <Route path="/" element={<E_main />}></Route>
+      <Route path="/detail_user" element={<E_main_detail />} />
+      <Route path="/detail_bookmark" element={<E_main_bookmark />} />
+    </Routes>
+  );
+};
+export default E_Routes;
