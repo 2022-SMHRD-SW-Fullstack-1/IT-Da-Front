c43e786069f7a352d17d7714c8c7619347b8060a
Brinicle177 <kimkr0421@naver.com>
2023-01-31 16:39:11 +0900

    학생 공지사항 디테일 페이지 추가

--- portal/src/routes/S_Routes.js
+++ portal/src/routes/S_Routes.js
@@ -12,6 +12,7 @@ import CoverLetterFrame from '../pages/Course/Student/Resume/CoverLetterFrame.js
 import C_extend from '../pages/Course/Teacher/extend/C_extend.js'
 import C_extend_detail from '../pages/Course/Teacher/extend/C_extend_detail.js'
 import C_announcement from '../pages/Course/C_announcement.js'
+import C_announcement_detail from '../pages/Course/C_announcement_detail';
 import C_archive from '../pages/Course/C_archive'
 
 const S_Routes = () => {
@@ -29,6 +30,7 @@ const S_Routes = () => {
          <Route path='/extend' element={<C_extend />}></Route>
          <Route path='/extend/detail' element={<C_extend_detail />}></Route>
          <Route path='/announcement' element={<C_announcement />}></Route>
+         <Route path='/announcement/detail' element={<C_announcement_detail />}/>
          <Route path='/archive' element={<C_archive />}></Route>
       </Routes>
    )
