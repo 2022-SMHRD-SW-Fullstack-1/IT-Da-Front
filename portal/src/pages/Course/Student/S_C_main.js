77a6c12d123931dbb57390b059bcc49c202018af
Min-SungJo <123or@naver.com>
2023-02-02 16:58:41 +0900

    기업 공고 필터 기능 추가

--- portal/src/pages/Course/Student/S_C_main.js
+++ portal/src/pages/Course/Student/S_C_main.js
@@ -12,18 +12,58 @@ const S_C_main = () => {
           .get("/enterprise/select_company")
           .then(function (res) {
               setCompany(res.data)
+              setNewCompany(res.data)
           })
           .catch(function (error) {
               console.log("error")
           })
   }, [])  
+      //필터기능
+      const onFilter = (f_company_name, f_company_area, f_company_position, f_company_salary) => {
+        console.log("필터내용", filter)
+        //newCourse리스트를 course에서 필터링 내용이 포함된 리스트로 구성
+        //지역, 직무, 급여, 이름
+        setNewCompany(company.filter(company =>
+            company.company_name.includes(f_company_name) &&
+            company.company_area.includes(f_company_area) &&
+            company.company_position.includes(f_company_position)&&
+            company.company_salary.includes(f_company_salary) 
+            ))
+    }
+      //필터input 태그 함수
+      const onChange = e => {
+        const { name, value } = e.target;
+        setFilter({
+            ...filter,
+            [name]: value
+        })
+    }
+    //필터관련 변수
+    const [filter, setFilter] = useState({
+        f_company_name: '',
+        f_company_area: '',
+        f_company_position: '',
+        f_company_salary: '',
+    })
+
+  const { f_company_name, f_company_area, f_company_position, f_company_salary } = filter
+
+  const [newCompany, setNewCompany] = useState([{
+    }])
 
   const companyList =
-      company.map((item) => <M_company_list item={item} key={item.company_name} />)
+    newCompany.map((item) => <M_company_list item={item} key={item.company_name} />)
 
   return (
       <div className='container'>
           <p>기업 공고</p>
+          <div className="filter_input_div">
+                <div><input type='text' name='f_company_name' placeholder='기업명으로 검색하기' onChange={onChange} /></div>
+                <div><input type='text' name='f_company_area' placeholder='지역으로 검색하기' onChange={onChange} /></div>
+                <div><input type='text' name='f_company_position' placeholder='희망지역으로 검색하기' onChange={onChange} /></div>
+                <div><input type='text' name='f_company_salary' placeholder='급여로 검색하기' onChange={onChange} /></div>
+                <div><button onClick={() => onFilter(f_company_name, f_company_area, f_company_position, f_company_salary)}>검색</button></div>
+            </div>
           <div className='content'>
               <table>
                   <thead>
